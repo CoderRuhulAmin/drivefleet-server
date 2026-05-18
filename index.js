dc32890425c077c20c714da8df6ab3d84a7c6ec5
CoderRuhulAmin <coderruhulamin@gmail.com>
2026-05-18 23:17:55 +0600

    CRUD API for Cars is created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -32,24 +32,101 @@ async function run() {
         const db = client.db('drivefleet-db');
         const carsCollection = db.collection('cars');
 
+        const verifyToken = (req, res, next) => {
+            const authHeader = req.headers.authorization;
+            console.log(authHeader);
+
+            if (!authHeader) {
+                return res.status(401).json({
+                    status: "error",
+                    message: "No authorization header found"
+                });
+            }
+
+            const token = authHeader.split(" ")[1];
+            console.log(token);
+
+            if (!token) {
+                return res.status(401).json({
+                    status: "error",
+                    message: "Authorization token not found!"
+                });
+            } else if (token !== "logged_in") {
+                return res.status(401).json({
+                    status: "error",
+                    message: "Authorization token not valid!"
+                });
+            }
+            next();
+        }
 
         app.get("/", (req, res) => {
             res.send("Server is running fine!");
         });
 
-        app.get('/cars', async (req, res) => {
-            const cars = [
-                {
-                    name: 'Car Name',
-                    rent: 5000
-                }
-            ];
+        app.get('/featured', async (req, res) => {
+            const cars = await carsCollection.find().limit(4).toArray();
+            res.json({
+                status: 'success',
+                data: cars
+            });
+        })
 
-            res.status(201).json({
+        app.get('/cars', async (req, res) => {
+            const cars = await carsCollection.find().toArray();
+
+            res.json({
                 status: 'success',
                 data: cars
             })
         })
+        app.post('/cars', async (req, res) => {
+            const newCarData = req.body;
+            // console.log("new Car Data: ", newCarData);
+
+            const cars = await carsCollection.insertOne(newCarData);
+            
+            res.json({
+                status: 'success',
+                data: cars
+            })
+        })
+        app.get('/cars/:id', async (req, res) => {
+            const { id } = req.params;
+
+            const car = await carsCollection.findOne({_id: new ObjectId(id)});
+
+            res.json({
+                status: 'success',
+                data: car
+            })
+        })
+        app.patch('/cars/:id', async (req, res) => {
+            const { id } = req.params;
+            const editedCarData = req.body;
+
+            const updatedCarData = await carsCollection.updateOne(
+                {_id: new ObjectId(id)},
+                {$set: editedCarData}
+            )
+
+            res.json({
+                status: 'success',
+                data: updatedCarData
+            })
+        })
+        app.delete('/cars/:id', async (req, res) => {
+            const { id } = req.params;
+
+            const result = await carsCollection.deleteOne({_id: new ObjectId(id)});
+
+            res.json({
+                status: 'success',
+                data: cars
+            })
+        })
+
+
 
 
 
